356d1cf21518d52f7affb2f4f14522dddf73c046
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-31 23:49:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -19,4 +19,9 @@ class App < Sinatra::Base
     "Goodbye #{@user_name}!"
   end
 
+  get "/multiply/:num1/:num2" do
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2]
+    "#{@num1}"
+  end
 end
